c4e91368ff5f38d7af15022c11f93225a024da98
AMANJEET KUMAR <80439933+Amanjeetk11@users.noreply.github.com>
2021-10-24 16:20:32 +0530

    Add files via upload

---
+++ about/about.php
@@ -0,0 +1,178 @@
+<!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,200;0,300;1,300&display=swap" rel="stylesheet">
+
+<link rel="stylesheet" type="text/css" href="style.css">
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark" >
+  <div class="container-fluid">
+    <!--<div class="logo"><img src="logo1.png"></div>-->
+    
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="../entry.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="about.php">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Contact</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Products
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#electronics" >Electronics</a></li>
+           
+            <li><a class="dropdown-item" href="#section1">Furnitures</a></li>
+            <li><a class="dropdown-item" href="#clothing">Clothing</a></li>
+          </ul>
+        </li>
+        <li class="logo"><a class="navbar-brand kol" href="#"><img src="images1/logo-white.png"></a></li>
+      </ul>
+      
+      <li class="nav-item dropdown">
+        <a class="log nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+         Login/ Signup
+        </a>
+        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <li><a class="dropdown-item" href="../index.php">User Login</a></li>
+          <li><a class="dropdown-item" href="../index.php">User Signup</a></li>
+          <li><hr class="dropdown-divider"></li>
+          <li><a class="dropdown-item" href="../index.php">Admin Login</a></li>
+          <li><a class="dropdown-item" href="../index.php">Admin Signup</a></li>
+          
+        </ul>
+      </li>
+
+    </div>
+  </div>
+
+</nav>
+
+<div class="second-nav">
+  <h3><b><a href="../entry.php">Home  </a>  <span><i class="fa fa-angle-right"></i>  </span>  <a href="about.php"> About</a></b></h3>
+</div>
+
+<div class="text">
+  <div class="red">
+    <h1>Red Store</h1>
+    <p>Red Store is a world renowned Online Shopping Store: Known for the perfection in products. Red Store Private Limited is an Indian e-commerce company established in 2007. It started with a primary focus on online book sales and soon, expanded to lifestyle products, electronics, home essentials and groceries. Today, Red Store is the biggest online Indian marketplace competing with the world leader Amazon.</p>
+  </div>
+</div>
+
+<div class="line"></div>
+<br><br>
+
+ <!--------------------------------testimonial----------------------------------->
+
+  <div class="testimonial">
+    <div class="small-container">
+      <div class="row">
+        <div class="col-3">
+          <i class="fa fa-quote-left"></i>
+          <p><b>I am so glad, to be a part of this amazing RedStore. It's been 5 years now and i didn't received any bad product. Like the best online shopping website ever.</b></p>
+          <div class="rating">
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+          </div>
+          <img src="images1/user-1.png">
+          <h3><b>Sean Parker</b></h3>
+        </div>
+        <div class="col-3">
+          <i class="fa fa-quote-left"></i>
+          <p><b>To be honest, I have been wasting time doing shoping from stores. It's so bazzared and frustrated at the same time. Now my life is so easy seriously Thanking you.</b></p>
+          <div class="rating">
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star-o"></i>
+          </div>
+          <img src="images1/user-2.png">
+          <h3><b>Dev Roy</b></h3>
+        </div>
+        <div class="col-3">
+          <i class="fa fa-quote-left"></i>
+          <p><b>I never liked to shop from Online shopping websites. It's is easy isn't it, I never trusted one until i became a member of this fantastic online shop. Every Product I have bough is same as they show in pictures, so satisfied.</b></p>
+          <div class="rating">
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+          </div>
+          <img src="images1/user-3.png">
+          <h3><b>Hermioni Granger</b></h3>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-----------------------------brands------------------------------------------------>
+
+  <div class="brands">
+    <div class="small-container">
+      <div class="row">
+        <div class="col-5">
+          <img src="images1/logo-godrej.png">
+        </div>
+        <div class="col-5">
+          <img src="images1/logo-oppo.png">
+        </div>
+        <div class="col-5">
+          <img src="images1/logo-coca-cola.png">
+        </div>
+        <div class="col-5">
+          <img src="images1/logo-paypal.png">
+        </div>
+        <div class="col-5">
+          <img src="images1/logo-philips.png">
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!----------------------------------------footer------------------------------------------>
+
+  <div class="footer">
+    <div class="container">
+      <div class="row" class="kol">
+        <div class="footer-col-2">
+          <img src="images1/logo-white.png">
+          <p>Our purpose is to Sustainably make the pleasure and Benefits of Sports Accessible to the many.</p>
+        </div>
+        <div class="footer-col-3">
+          <h3>Useful Links</h3>
+          <ul>
+            <li>Coupons</li>
+            <li>Blog Post</li>
+            <li>Return Policy</li>
+            <li>Join Affiliate</li>
+          </ul>
+        </div>
+        <div class="footer-col-4">
+          <h3>Follow us</h3>
+          <ul>
+            <li>Facebook</li>
+            <li>Twitter</li>
+            <li>Instagram</li>
+            <li>Youtube</li>
+          </ul>
+        </div>
+      </div>
+      <hr>
+      <p class="copyright">Copyright © 2010-2021 Freepik Company S.L. All Rights Reserved.</p>
+    </div>
+  </div>
