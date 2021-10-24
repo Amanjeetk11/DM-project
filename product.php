2ddb00532d56fff263c3d2e29fe74788b7bf8589
AMANJEET KUMAR <80439933+Amanjeetk11@users.noreply.github.com>
2021-10-24 23:53:57 +0530

    Update product.php

--- product.php
+++ product.php
@@ -59,6 +59,15 @@ if(isset($_GET['addtocart'])){
  <!DOCTYPE html>  
  <html>  
       <head>  
+       <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-J59SMH3MJ7"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-J59SMH3MJ7');
+</script>
            <title>Products</title>  
            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
           <!-- JavaScript Bundle with Popper -->
